39da6d2693f257f441ac5d096e91e6c9b56b9b84
zengyonghua <zengyonghua@qq.com>
2022-03-04 11:52:06 +0800

    ..

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author: zyh
+ * @date: 2022/3/4
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Nice");
+    }
+}
