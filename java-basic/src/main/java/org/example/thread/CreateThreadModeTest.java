3f064bc1b31612a6a3338c19675fe0904392b7af
zengyonghua <zengyonghua@qq.com>
2022-03-05 15:10:40 +0800

    Add ThreadPool

--- java-basic/src/main/java/org/example/thread/CreateThreadModeTest.java
+++ java-basic/src/main/java/org/example/thread/CreateThreadModeTest.java
@@ -23,7 +23,7 @@ public class CreateThreadModeTest {
     }
 
     @Test
-    public void createModelTwo(){
+    public void createModelTwo() {
         Thread twoMode = new Thread(new TwoCreateThreadMode());
         twoMode.setName("线程2");
         twoMode.start();
@@ -38,7 +38,7 @@ public class CreateThreadModeTest {
     }
 
     @Test
-    public void createModeFour(){
+    public void createModeFour() {
         FourCreateThreadMode fourCreateThreadMode = new FourCreateThreadMode();
         ExecutorService executorService = fourCreateThreadMode.getExecotorService();
         executorService.submit(new OneCreateThreadMode());
@@ -51,6 +51,11 @@ public class CreateThreadModeTest {
  * 第一种创建线程方式：继承Thread
  * <p>
  * 重写run()方法
+ * <p>
+ * 缺点：
+ * 1、每次都创建线程对象性能差
+ * 2、线程缺乏统一管理，可能无线创建线程，相互之间竞争，即可能占用过多系统资源导致死机或oom（内存用完了 Out Of Memory）
+ * 3、缺乏更多功能: 如定时执行、定期执行、线程中断
  */
 class OneCreateThreadMode extends Thread {
 
@@ -77,6 +82,8 @@ class OneCreateThreadMode extends Thread {
     public void execute() {
         System.out.println(Thread.currentThread().getName() + ":" + "execute");
     }
+
+
 }
 
 /**
@@ -135,11 +142,64 @@ class ThreeCreateThreadMode implements Callable<Integer> {
 
 /**
  * 第四种创建线程方式：创建线程池 ExecutorService
+ * <p>
+ * 线程池优点：
+ * 1. 重用存在的线程，减少对象创建、消亡的开销，性能佳。
+ * 2. 可有效控制最大并发线程数，提高系统资源的使用率，同时避免过多资源竞争，避免堵塞。
+ * 3. 提供定时执行、定期执行、单线程、并发数控制等功能。
  */
 class FourCreateThreadMode {
 
-    public ExecutorService getExecotorService(){
+    public ExecutorService getExecotorService() {
         return Executors.newSingleThreadExecutor();
     }
+
+    /**
+     * 可缓存线程池，如果线程池长度超过处理需要，可灵活回收空闲线程，若无可回收，则新建线程。
+     * 线程池为无限大，当执行第二个任务时第一个任务已经完成，会复用执行第一个任务的线程，而不用每次新建线程。
+     *
+     * @return
+     */
+    public ExecutorService newCachedThreadPool() {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        return executorService;
+    }
+
+    /**
+     * 创建一个定长线程池，可控制线程最大并发数，超出的线程会在队列中等待
+     *
+     * @param size 线程池大小
+     * @return
+     */
+    public ExecutorService newFixedThreadPool(int size) {
+        ExecutorService executorService = Executors.newFixedThreadPool(size);
+        return executorService;
+    }
+
+    /**
+     * 创建一个定长线程池，支持定时及周期性任务执行
+     *
+     * @param size
+     * @return
+     */
+    public ScheduledExecutorService newScheduledThreadPool(int size) {
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(size);
+        return scheduledExecutorService;
+    }
+
+    /**
+     * 创建一个单线程化的线程池，它只会用唯一的工作线程来执行任务，保证所有任务按照指定顺序(FIFO, LIFO, 优先级)执行
+     *
+     * 结果依次输出，相当于顺序执行各个任务。
+     * 现行大多数GUI程序都是单线程的。
+     * Android中单线程可用于数据库操作，文件操作，应用批量安装，应用批量删除等不适合并发但可能IO阻塞性及影响UI线程响应的操作
+     *
+     * @return
+     */
+    public ExecutorService newSingleThreadExecutor() {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        return executorService;
+    }
+
 
 }
