698ae6d31a731b248e6be423e179fb485ce4b38d
zengyonghua <zengyonghua@qq.com>
2022-03-07 15:48:34 +0800

    New Add Interface Test

--- java-basic/src/main/java/org/example/inter/Duck.java
+++ java-basic/src/main/java/org/example/inter/Duck.java
@@ -4,7 +4,12 @@ package org.example.inter;
  * @author: zyh
  * @date: 2022/3/7
  */
-public class Duck extends AbstractAnimal{
+public class Duck extends AbstractAnimal implements Animal{
+
+    @Override
+    public void eat(String name) {
+        System.out.println(name+"正在吃...鱼仔");
+    }
 
     public static void main(String[] args) {
         Duck duck = new Duck();
