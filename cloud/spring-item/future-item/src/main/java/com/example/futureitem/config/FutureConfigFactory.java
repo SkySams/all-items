3d43e433584fd2aa5bb29eb78d917c0b1d9c03b8
zengyonghua <zengyonghua@qq.com>
2022-03-08 18:26:09 +0800

    New Add Nacos

---
+++ cloud/spring-item/future-item/src/main/java/com/example/futureitem/config/FutureConfigFactory.java
@@ -0,0 +1,19 @@
+package com.example.futureitem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author: zyh
+ * @date: 2022/3/8
+ */
+@Configuration
+public class FutureConfigFactory {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
