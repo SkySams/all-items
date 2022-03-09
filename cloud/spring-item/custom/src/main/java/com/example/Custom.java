91bed3ebdfd6c51e534cdf0704ddfffdff958392
zengyonghua <zengyonghua@qq.com>
2022-03-09 17:47:26 +0800

    ..

---
+++ cloud/spring-item/custom/src/main/java/com/example/Custom.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: zyh
+ * @date: 2022/3/9
+ */
+@SpringBootApplication
+public class Custom {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Custom.class,args);
+    }
+
+}
