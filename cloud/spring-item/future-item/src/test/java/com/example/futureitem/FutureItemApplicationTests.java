4b1278aae74ae03c9953fa68b5fff288c4e8516c
zengyonghua <zengyonghua@qq.com>
2022-03-11 09:20:46 +0800

    ..

---
+++ cloud/spring-item/future-item/src/test/java/com/example/futureitem/FutureItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.futureitem;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FutureItemApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
