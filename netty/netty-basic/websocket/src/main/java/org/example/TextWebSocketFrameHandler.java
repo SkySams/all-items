1f0fdcb40e57708263a8db5ffbfeaa70130b9b46
zengyonghua <zengyonghua@qq.com>
2022-03-24 17:15:42 +0800

    New Add WebSocket

---
+++ netty/netty-basic/websocket/src/main/java/org/example/TextWebSocketFrameHandler.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+
+/**
+ * @author: zyh
+ * @date: 2022/3/24
+ */
+public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    private final ChannelGroup group;
+
+    public TextWebSocketFrameHandler(ChannelGroup group) {
+        this.group = group;
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx,
+                                   Object evt) throws Exception {
+        if (evt == WebSocketServerProtocolHandler.ServerHandshakeStateEvent.HANDSHAKE_COMPLETE) {
+            ctx.pipeline().remove(HttpRequestHandler.class);
+            group.writeAndFlush(new TextWebSocketFrame("Client " + ctx.channel() + " joined"));
+            group.add(ctx.channel());
+        } else {
+            super.userEventTriggered(ctx, evt);
+        }
+    }
+
+
+    /**
+     * Is called for each message of type {}.
+     *
+     * @param ctx the {@link ChannelHandlerContext} which this {@link SimpleChannelInboundHandler}
+     *            belongs to
+     * @param msg the message to handle
+     * @throws Exception is thrown if an error occurred
+     */
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        group.writeAndFlush(msg.retain());
+    }
+
+}
