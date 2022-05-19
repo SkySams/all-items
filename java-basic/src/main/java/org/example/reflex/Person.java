b52b2f3a7c03b41808506cdd513c74edccb7ca46
zengyonghua <zengyonghua@qq.com>
2022-05-19 17:13:42 +0800

    JAVA 反射

---
+++ java-basic/src/main/java/org/example/reflex/Person.java
@@ -0,0 +1,13 @@
+package org.example.reflex;
+
+/**
+ * @author: zyh
+ * @date: 2022/5/19
+ */
+public class Person {
+
+    static {
+        System.out.println("hello person");
+    }
+
+}
--- java-basic/src/main/java/org/example/reflex/TestReflex.java
+++ java-basic/src/main/java/org/example/reflex/TestReflex.java
@@ -1,13 +1,9 @@
 package org.example.reflex;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import org.example.annotation.Test;
+import org.junit.Test;
 
-import java.math.BigDecimal;
+import java.io.FileReader;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.Collectors;
 
 /**
  * https://blog.csdn.net/qq_44715943/article/details/120587716
@@ -69,15 +65,66 @@ public class TestReflex {
 
     }
 
+
+    @Test
+    public void test_five(){
+        ResourceBundle bundle = ResourceBundle.getBundle("javase/reflectBean/db");
+        String className = bundle.getString("className");
+        System.out.println(className);
+
+    }
+
+
+    /**
+     *  如果你只是希望一个类的静态代码块执行，其它代码一律不执行，
+     *  你可以使用：
+     *  Class.forName("完整类名");
+     */
+    @Test
+    public void test_four(){
+        try {
+            Class.forName("org.example.reflex.Person");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void test_three() throws Exception {
+        // 以下代码是灵活的，代码不需要改动，可以修改配置文件，配置文件修改之后，可以创建出不同的实例对象。
+        // 通过IO流读取reflectClassInfo.properties文件
+        FileReader reader = new FileReader("D:\\workspace\\all-items\\java-basic\\src\\main\\java\\reflectClassInfo.properties");
+        // 创建属性类对象Map
+        // key value都是String
+        Properties pro = new Properties();
+        //加载
+        pro.load(reader);
+        reader.close();
+        // 通过key获取value
+        String className = pro.getProperty("MyReflex");
+        // 通过反射机制实例化对象
+        Class c = Class.forName(className);
+        Object obj = c.newInstance();
+        System.out.println(obj);
+
+
+    }
+
     /**
      * 获取到Class，能干什么？
      * 通过Class的newInstance()方法来实例化对象。
      * 注意：newInstance()方法内部实际上调用了无参数构造方法，必须保证无参构造存在才可以。
      */
+    @Test
     public void test_two() {
         try {
-//            Class cla = Class.forName("org.example.reflex.MyReflex");
-            Class cla = Class.forName("org.example.reflex.TestReflex", true, this.getClass().getClassLoader());
+            Class cla = Class.forName("org.example.reflex.MyReflex");
+//            Class cla = Class.forName("org.example.reflex.TestReflex", true, this.getClass().getClassLoader());
+            Object obj = cla.newInstance();
+            MyReflex myReflex = new MyReflex();
+            System.out.println(myReflex);
+            System.out.println(obj);
             System.out.println(cla);
         } catch (Exception e) {
             e.printStackTrace();
