99296ce23446139fe46bfc143068f174befe301f
SkySams <1152610087@qq.com>
2022-11-02 21:25:52 +0800

    ..

--- starter/src/test/java/org/test/JwtTest.java
+++ starter/src/test/java/org/test/JwtTest.java
@@ -15,29 +15,29 @@ import java.util.Date;
  * @date: 2022/3/23
  */
 public class JwtTest {
-    private static final byte[] SECRET = "6MNSobBRCHGIO0fS6MNSobBRCHGIO0fS".getBytes();
-    private static final long EXPIRE_TIME = 1000 * 5;
-
-
-    public static void main(String[] args) throws JOSEException {
-        MACSigner macSigner = new MACSigner(SECRET);
-        /**
-         * 2. 建立payload 载体
-         */
-        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
-                .subject("doi")
-                .issuer("http://www.doiduoyi.com")
-                .expirationTime(new Date(System.currentTimeMillis() + EXPIRE_TIME))
-                .claim("ACCOUNT","123456")
-                .build();
-
-        /**
-         * 3. 建立签名
-         */
-        SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
-        signedJWT.sign(macSigner);
-        System.out.println(signedJWT.serialize());
-
-    }
+//    private static final byte[] SECRET = "6MNSobBRCHGIO0fS6MNSobBRCHGIO0fS".getBytes();
+//    private static final long EXPIRE_TIME = 1000 * 5;
+//
+//
+//    public static void main(String[] args) throws JOSEException {
+//        MACSigner macSigner = new MACSigner(SECRET);
+//        /**
+//         * 2. 建立payload 载体
+//         */
+//        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+//                .subject("doi")
+//                .issuer("http://www.doiduoyi.com")
+//                .expirationTime(new Date(System.currentTimeMillis() + EXPIRE_TIME))
+//                .claim("ACCOUNT","123456")
+//                .build();
+//
+//        /**
+//         * 3. 建立签名
+//         */
+//        SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
+//        signedJWT.sign(macSigner);
+//        System.out.println(signedJWT.serialize());
+//
+//    }
 
 }
