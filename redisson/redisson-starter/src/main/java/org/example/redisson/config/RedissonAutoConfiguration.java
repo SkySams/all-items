50fb68fce72fd2b5355014d964ed7657db143185
zengyonghua <zengyonghua@qq.com>
2023-01-19 18:28:31 +0800

    ..

--- redisson/redisson-starter/src/main/java/org/example/redisson/config/RedissonAutoConfiguration.java
+++ redisson/redisson-starter/src/main/java/org/example/redisson/config/RedissonAutoConfiguration.java
@@ -1,24 +1,24 @@
 package org.example.redisson.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.redisson.config.TransportMode;
-import org.redisson.spring.cache.CacheConfig;
-import org.redisson.spring.cache.RedissonSpringCacheManager;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * @author: zyh
  * @date: 2022/3/4
  */
+@Slf4j
 @ConditionalOnClass(Redisson.class)
 @EnableConfigurationProperties(RedissonAutoConfiguration.class)
 @Configuration
@@ -38,7 +38,40 @@ public class RedissonAutoConfiguration {
         config.useSingleServer()
                 .setAddress(prefix+redissonProperties.getHost()+":"+redissonProperties.getPort())
                 .setConnectTimeout(redissonProperties.getTimeout());
+        log.info("Redisson 配置参数", redissonProperties);
         return Redisson.create(config);
+    }
+
+    /**
+     * spring cache 缓存, 值由默认的二进制改为使用json字符串
+     */
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration(CacheProperties cacheProperties) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig();
+
+        redisCacheConfiguration = redisCacheConfiguration.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
+        redisCacheConfiguration = redisCacheConfiguration.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        // 不加以下配置的话, ttl等spring cache的配置不会起作用
+        CacheProperties.Redis redisProperties = cacheProperties.getRedis();
+        if (redisProperties.getTimeToLive() != null) {
+            redisCacheConfiguration = redisCacheConfiguration.entryTtl(redisProperties.getTimeToLive());
+        }
+
+        if (redisProperties.getKeyPrefix() != null) {
+            redisCacheConfiguration = redisCacheConfiguration.prefixCacheNameWith(redisProperties.getKeyPrefix());
+        }
+
+        if (!redisProperties.isCacheNullValues()) {
+            redisCacheConfiguration = redisCacheConfiguration.disableCachingNullValues();
+        }
+
+        if (!redisProperties.isUseKeyPrefix()) {
+            redisCacheConfiguration = redisCacheConfiguration.disableKeyPrefix();
+        }
+
+        log.info(" spring cache 缓存, 值由默认的二进制改为使用json字符串");
+        return redisCacheConfiguration;
     }
 
 }
