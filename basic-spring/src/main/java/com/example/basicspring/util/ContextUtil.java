8994bc634723825c28f00925ee557a63aa17c0ee
SkySams <1152610087@qq.com>
2023-02-02 21:21:51 +0800

    ..

--- basic-spring/src/main/java/com/example/basicspring/util/ContextUtil.java
+++ basic-spring/src/main/java/com/example/basicspring/util/ContextUtil.java
@@ -12,17 +12,21 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public final class ContextUtil implements ApplicationContextAware {
-    protected static ApplicationContext applicationContext ;
+
+    protected static ApplicationContext applicationContext;
+
     @Override
     public void setApplicationContext(ApplicationContext arg0) throws BeansException {
         if (applicationContext == null) {
             applicationContext = arg0;
         }
     }
+
     public static Object getBean(String name) {
         //name表示其他要注入的注解name名
         return applicationContext.getBean(name);
     }
+
     /**
      * 拿到ApplicationContext对象实例后就可以手动获取Bean的注入实例对象
      */
